d2125efa4c94d0dcb634da4da2469c326ce99ac9
corelate <dev.corelate@gmail.com>
2026-05-02 21:29:15 +0800

    Add targetField compatibility accessors for certificate mapping

--- src/main/java/com/corelate/app/repository/MockAppRepository.java
+++ src/main/java/com/corelate/app/repository/MockAppRepository.java
@@ -1,6 +1,7 @@
 package com.corelate.app.repository;
 
 import com.corelate.app.entity.MockApp;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,6 +10,13 @@ import java.util.Optional;
 public interface MockAppRepository extends JpaRepository<MockApp, Long> {
 
     Optional<MockApp> findByAppId(String appId);
+
+    @EntityGraph(attributePaths = "certificateFieldMappings")
+    Optional<MockApp> findWithCertificateFieldMappingsByAppId(String appId);
+
+    @Override
+    @EntityGraph(attributePaths = "certificateFieldMappings")
+    List<MockApp> findAll();
 
     List<MockApp> findByAppIdIn(List<String> appIds);
 }
